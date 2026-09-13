c1785bca19eef3a991ef7eb07cffb9b7863d9c88
welingthonalvesnobrega27-beep <welingthonalvesnobrega27@gmail.com>
2026-09-12 22:10:55 -0300

    troca modelo groq novo

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def chat():
             return jsonify({"reply": "Manda uma mensagem ai"})
 
         completion = client.chat.completions.create(
-            model="llama-3.3-70b-versatile",
+            model="openai/gpt-oss-20b",
             messages=[
                 {"role": "system", "content": "Você é o NEXO, uma IA útil, direta e brasileira."},
                 {"role": "user", "content": user_msg}
